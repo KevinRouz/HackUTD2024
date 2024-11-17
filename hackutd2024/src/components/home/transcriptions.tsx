17c8879a82a40575cac7a9bb9b9cd43bc3c56a13
BlueStarBurst <hargreaves.bryant@gmail.com>
2024-11-17 01:25:08 -0600

    local env

--- hackutd2024/src/components/home/transcriptions.tsx
+++ hackutd2024/src/components/home/transcriptions.tsx
@@ -1,4 +1,14 @@
+"use client";
+
+import { getAllFiles } from "@/lib/server/python";
+import { useEffect } from "react";
+
 export default function Transcriptions() {
+
+    useEffect(() => {
+        getAllFiles();
+    }, []);
+
     return (
         <div className="w-full">
             
--- hackutd2024/src/lib/server/python.ts
+++ hackutd2024/src/lib/server/python.ts
@@ -4,12 +4,16 @@ import { PinataSDK } from "pinata";
 
 
 
-export function getAllFiles() {
+export async function getAllFiles() {
 
     const pinata = new PinataSDK({
         pinataJwt: process.env.PINATA_JWT,
         pinataGateway: "indigo-historical-bison-993.mypinata.cloud",
     });
+
+    const files = await pinata.files.list();
+
+    console.log(files);
 
     //   return fs.readdirSync(path.join(__dirname, 'python'));
 }
